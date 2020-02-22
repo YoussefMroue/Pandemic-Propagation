2172fad2984cc95e52f90c36136536aca9c88df0
Vaishall Pradeepkumar <mastervaishall@gmail.com>
2020-02-21 21:16:16 -0600

    Added logic

--- static/js/control.js
+++ static/js/control.js
@@ -1,43 +1,81 @@
-cases = d3.json("/api/cases").then(function(response) {return response;});
-deaths = d3.json("/api/deaths").then(function(response) {return response;});
-start_dates = []
+start_dates = [];
+SARS_button = d3.select("#SARS");
+H1N1_button = d3.select("#H1N1");
+Coronavirus_button = d3.select("#Coronavirus");
+active_buttons = [];
 
-var SARS_button = d3.select("#SARS");
-var H1N1_button = d3.select("#H1N1");
-var Coronavirus_button = d3.select("#Coronavirus");
-var active_buttons = [];
-var new_cases = cases;
+d3.json("/api/cases").then(function(response) {
+	og_cases = response;
+	cases = og_cases;
 
-SARS_button.on("click", function(){
-	if (active_buttons.includes('SARS')){
-		active_buttons = removeValue(active_buttons, 'SARS');
-		new_cases = filterViruses(cases);
-	}
-	else{
-		active_buttons.push('SARS');
-		new_cases = filterViruses(cases);
-	}
+	SARS_button.on("click", function(){
+		if (active_buttons.includes('SARS')){
+			active_buttons = removeValue(active_buttons, 'SARS');
+			cases = og_cases.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('SARS');
+			cases = og_cases.filter(filterViruses);
+		}
+	});
+	H1N1_button.on("click", function(){
+		if (active_buttons.includes('H1N1')){
+			active_buttons = removeValue(active_buttons, 'H1N1');
+			cases = og_cases.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('H1N1');
+			cases = og_cases.filter(filterViruses);
+		}
+	});
+	Coronavirus_button.on("click", function(){
+		if (active_buttons.includes('Coronavirus')){
+			active_buttons = removeValue(active_buttons, 'Coronavirus');
+			cases = og_cases.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('Coronavirus');
+			cases = og_cases.filter(filterViruses);
+		}
+	});
 });
-H1N1_button.on("click", function(){
-	if (active_buttons.includes('H1N1')){
-		active_buttons = removeValue(active_buttons, 'H1N1');
-		new_cases = filterViruses(cases);
-	}
-	else{
-		active_buttons.push('H1N1');
-		new_cases = filterViruses(cases);
-	}
+
+d3.json("/api/deaths").then(function(response) {
+	og_deaths = response;
+	deaths = og_deaths;
+
+	SARS_button.on("click", function(){
+		if (active_buttons.includes('SARS')){
+			active_buttons = removeValue(active_buttons, 'SARS');
+			deaths = og_deaths.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('SARS');
+			deaths = og_deaths.filter(filterViruses);
+		}
+	});
+	H1N1_button.on("click", function(){
+		if (active_buttons.includes('H1N1')){
+			active_buttons = removeValue(active_buttons, 'H1N1');
+			deaths = og_deaths.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('H1N1');
+			deaths = og_deaths.filter(filterViruses);
+		}
+	});
+	Coronavirus_button.on("click", function(){
+		if (active_buttons.includes('Coronavirus')){
+			active_buttons = removeValue(active_buttons, 'Coronavirus');
+			deaths = og_deaths.filter(filterViruses);
+		}
+		else{
+			active_buttons.push('Coronavirus');
+			deaths = og_deaths.filter(filterViruses);
+		}
+	});
 });
-Coronavirus_button.on("click", function(){
-	if (active_buttons.includes('Coronavirus')){
-		active_buttons = removeValue(active_buttons, 'Coronavirus');
-		new_cases = filterViruses(cases);
-	}
-	else{
-		active_buttons.push('Coronavirus');
-		new_cases = filterViruses(cases);
-	}
-});
+
 
 function removeValue(arr, value){
 	return arr.filter(function(element){
@@ -45,8 +83,6 @@ function removeValue(arr, value){
 	});
 }
 
-function filterViruses(country, active_buttons){
-	return active_buttons.includes(country.virus)
+function filterViruses(country){
+	return active_buttons.includes(country.Virus)
 }
-
-console.log(cases);
